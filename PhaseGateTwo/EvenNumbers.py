a808e027cdc00aeb3aee5a971f761b7b79dc437b
Victor <oladimejivictor611@gmail.com>
2024-10-04 14:57:51 +0100

    update

--- PhaseGateTwo/EvenNumbers.py
+++ PhaseGateTwo/EvenNumbers.py
@@ -1,6 +1,12 @@
-for count in range(1000,3000):
+for count in range(2000,3000):
 	if count%2==0:
-		print(count,end=',')
+		number = count
+		digit1 = number%10
+		digit2 = number//10
+		digit3 = digit2%10
+		digit4 = digit2//10
+		if digit1%2==0 and digit2%2==0 and digit3%2==0 and digit4%2==0:
+			print(count,end=',')
 
 
 
