e7eb45fcf3c91a45ba1a7187fecc322e359a8393
Victor <oladimejivictor611@gmail.com>
2024-10-04 15:20:28 +0100

    update

--- PhaseGateTwo/TaskFour.py
+++ PhaseGateTwo/TaskFour.py
@@ -1,12 +1,9 @@
 number = [23,45,12,14,11,46,21,47,33,1]
 def length_of_list():
 	length = 0
-	sum = number
 	for count in number:
 		length+=1
-	#return length
-	for count in range(sum,2):
-		count = number
-	return count
+	return length
 print(length_of_list())
-	
+list = number(1,10,2)
+print(list)
